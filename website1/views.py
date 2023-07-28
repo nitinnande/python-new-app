6393032082fc2d0480d38f6fcde0a4b23aebedeb
nitin <yogeshnande39@gmail.com>
2023-07-28 02:26:48 -0700

    file updated

--- website1/views.py
+++ website1/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
    
 
 def fun(request):
-    return HttpResponse('<h1>hello Ashok it at hydyrabad<h1/>')
+    return HttpResponse('<h1>hello Ashok it at hydyrabad city <h1/>')
 
 
