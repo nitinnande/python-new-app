1caea1640162bec03f93cc16237b1f0147712ae5
nitinnande <139989782+nitinnande@users.noreply.github.com>
2023-07-28 02:18:50 -0700

    Update views.py

--- website1/views.py
+++ website1/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
    
 
 def fun(request):
-    return HttpResponse('<h1>hello Ashok it<h1/>')
+    return HttpResponse('<h1>hello Ashok it at hydyrabad<h1/>')
 
 
