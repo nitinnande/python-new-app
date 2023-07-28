58f4db2f9a5aad820551b7dd0e6a8677d938188d
nitinnande <139989782+nitinnande@users.noreply.github.com>
2023-07-28 02:21:05 -0700

    Update views.py

--- website2/views.py
+++ website2/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def fun1(request):
-    return HttpResponse('<h1>hello django sdfersgrshrpy manage.py runserver<h1/>')
+    return HttpResponse('<h1>hello django manage.py runserver<h1/>')
